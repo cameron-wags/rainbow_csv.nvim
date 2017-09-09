37bbf412141531960b9439576c892a15f8a5918b
mechatroner <mechatroner@yandex.ru>
2017-09-09 16:36:03 -0400

    adding CLI unit tests

--- python/test_all.sh
+++ python/test_all.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+
 python -m unittest test_rbql
 python3 -m unittest test_rbql
 node ./unit_tests.js
+
+
+#Some CLI tests:
+md5sum_test=($( ./rbql.py --query "select a1,a2,a7,b2,b3,b4 left join test_datasets/countries.tsv on a2 == b1 where 'Sci-Fi' in a7.split('|') and b2!='US' and int(a4) > 2010" < test_datasets/movies.tsv | md5sum))
+md5sum_canonic=($( md5sum unit_tests/canonic_result_4.tsv ))
+if [ "$md5sum_canonic" != "$md5sum_test" ] ; then
+    echo "CLI test FAIL!"  1>&2
+fi
+
+md5sum_test=($( ./rbql.py --query "select a1,a2,a7,b2,b3,b4 left join test_datasets/countries.tsv on a2 == b1 where a7.split('|').includes('Sci-Fi') && b2!='US' && a4 > 2010" --meta_language js < test_datasets/movies.tsv | md5sum))
+md5sum_canonic=($( md5sum unit_tests/canonic_result_4.tsv ))
+if [ "$md5sum_canonic" != "$md5sum_test" ] ; then
+    echo "CLI test FAIL!"  1>&2
+fi
