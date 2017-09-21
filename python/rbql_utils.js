19d6c5e676688708be5dd410aba8743b668ee404
mechatroner <mechatroner@yandex.ru>
2017-09-21 02:52:03 -0400

    rfc-compliant implementation of csv split in js

--- python/rbql_utils.js
+++ python/rbql_utils.js
@@ -1,35 +1,44 @@
 function split_escaped_csv_str(src) {
     if (src.indexOf('"') == -1)
-        return src.split(',')
+        return [src.split(','), false];
     var result = [];
-    var cidx = 0
-    if (src.charAt(0) === ',') {
-        result.push('');
-        cidx = 1;
-    }
+    var warning = false;
+    var cidx = 0;
     while (cidx < src.length) {
         if (src.charAt(cidx) === '"') {
-            var uidx = src.indexOf('",', cidx + 1);
-            if (uidx != -1) {
-                uidx += 1;
-            } else if (uidx == -1 && src.charAt(src.length - 1) == '"') {
+            var uidx = cidx + 1;
+            while (true) {
+                uidx = src.indexOf('"', uidx);
+                if (uidx == -1) {
+                    result.push(src.substring(cidx + 1).replace('""', '"'));
+                    return [result, true];
+                } else if (uidx + 1 >= src.length || src.charAt(uidx + 1) == ',') {
+                    result.push(src.substring(cidx + 1, uidx).replace('""', '"'));
+                    cidx = uidx + 2;
+                    break;
+                } else if (src.charAt(uidx + 1) == '"') {
+                    uidx += 2; 
+                    continue;
+                } else {
+                    warning = true;
+                    uidx += 1;
+                    continue;
+                }
+            }
+        } else {
+            var uidx = src.indexOf(',', cidx);
+            if (uidx == -1)
                 uidx = src.length;
-            }
-            if (uidx != -1) {
-                result.push(src.substring(cidx + 1, uidx - 1));
-                cidx = uidx + 1;
-                continue;
-            }
+            var field = src.substring(cidx, uidx);
+            if (field.indexOf('"') != -1)
+                warning = true;
+            result.push(field);
+            cidx = uidx + 1;
         }
-        var uidx = src.indexOf(',', cidx);
-        if (uidx == -1)
-            uidx = src.length;
-        result.push(src.substring(cidx, uidx));
-        cidx = uidx + 1;
     }
     if (src.charAt(src.length - 1) == ',')
         result.push('');
-    return result;
+    return [result, warning];
 }
 
 module.exports.split_escaped_csv_str = split_escaped_csv_str;
