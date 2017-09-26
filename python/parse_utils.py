f2f2dd8282a05ff0ff104f12b945cdee65b71c08
mechatroner <mechatroner@yandex.ru>
2017-09-26 19:50:36 -0400

    adding separate_actions regex-based function

--- python/parse_utils.py
+++ python/parse_utils.py
@@ -5,9 +5,24 @@ import re
 import unittest
 
 
+UPDATE = 'UPDATE'
+SELECT = 'SELECT'
+SELECT_TOP = 'SELECT TOP'
+SELECT_DISTINCT = 'SELECT DISTINCT'
+JOIN = 'JOIN'
+INNER_JOIN = 'INNER JOIN'
+LEFT_JOIN = 'LEFT JOIN'
+STRICT_LEFT_JOIN = 'STRICT LEFT JOIN'
+ORDER_BY = 'ORDER BY'
+WHERE = 'WHERE'
+
+
+class RBParsingError(Exception):
+    pass
+
+
 def replace_string_literals(rbql_expression):
     # regex is improved expression from here: https://stackoverflow.com/a/14366904/2898283
-    #FIXME what if string has "r" prefix ???
     matches = list(re.finditer(r'''(\"\"\"|\'\'\'|\"|\')((?<!\\)(\\\\)*\\\1|.)*?\1''', rbql_expression))
     string_literals = list()
     format_parts = list()
@@ -23,6 +38,60 @@ def replace_string_literals(rbql_expression):
     return (format_expression, string_literals)
 
 
+def separate_actions(rbql_expression):
+    #TODO implement SELECT_TOP_DISTINCT
+    statement_groups = list()
+    statement_groups.append([STRICT_LEFT_JOIN, LEFT_JOIN, INNER_JOIN, JOIN])
+    statement_groups.append([SELECT_DISTINCT, SELECT_TOP, SELECT])
+    statement_groups.append([ORDER_BY])
+    statement_groups.append([WHERE])
+    statement_groups.append([UPDATE])
+
+    result = dict()
+
+    ordered_statements = list()
+
+    for st_group in statement_groups:
+        for statement in st_group:
+            rgxp = None
+            if statement == SELECT_TOP:
+                rgxp = r'(?i)(?:^|[ ])SELECT *TOP *([0-9][0-9]*)(?:$|[ ])'
+            else:
+                rgxp = r'(?i)(?:^|[ ]){}(?:$|[ ])'.format(statement.replace(' ', ' *'))
+            matches = list(re.finditer(rgxp, rbql_expression))
+            if not len(matches):
+                continue
+            if len(matches) > 1:
+                raise RBParsingError('More than one "{}" statements found'.format(statement))
+            assert len(matches) == 1
+            match = matches[0]
+            result[statement] = dict()
+            if statement == SELECT_TOP:
+                result[statement]['top'] = int(match.group(1))
+            ordered_statements.append((match.start(), match.end(), statement))
+            break #there must be only one statement maximum in each group
+
+    ordered_statements = sorted(ordered_statements)
+    for i in range(len(ordered_statements)):
+        statement_start = ordered_statements[i][0]
+        span_start = ordered_statements[i][1]
+        statement = ordered_statements[i][2]
+        span_end = ordered_statements[i + 1][0] if i + 1 < len(ordered_statements) else len(rbql_expression)
+        assert statement_start < span_start
+        assert span_start <= span_end
+        span = rbql_expression[span_start:span_end]
+        if statement == ORDER_BY:
+            result[statement]['reverse'] = False
+            span = re.sub('(?i)[ ]ASC[ ]*$', '', span)
+            new_span = re.sub('(?i)[ ]DESC[ ]*$', '', span)
+            if new_span != span:
+                span = new_span
+                result[statement]['reverse'] = True
+        result[statement]['text'] = span.strip()
+    return result
+
+
+
 class TestParsing(unittest.TestCase):
 
     def test_literals_replacement(self):
@@ -33,15 +102,14 @@ class TestParsing(unittest.TestCase):
         test_cases.append((r"Select 'hello', 100 order by a1 desc", ["'hello'"]))
         test_cases.append((r'Select "hello", *, "world" 100 order by a1 desc', ['"hello"', '"world"']))
         test_cases.append((r'Select "hello", "world", "hello \" world", "hello \\\" world", "hello \\\\\\\" world" order by "world"', ['"hello"', '"world"', r'"hello \" world"', r'"hello \\\" world"', r'"hello \\\\\\\" world"', '"world"']))
-        #test_cases.append((r"""Select 'hello', 'world', 'hello\' world', 'hello \\\' world', 'hello \\\\\\\' world' order by 'world'""",[]))
-        #test_cases.append((r'''Select "  hello 'world'  ", '  hello "world"  ' order by a1''',[]))
-        #test_cases.append((r'Select r"hello", r"world", "hello\" world", "hello \\\" world", "hello \\\\\\\" world \\\\\\\" world" order by "world"',[]))
-        #test_cases.append((r'Select "hello", "world", "hello\" world", "hello world\\\\\\", "hello world\\" order by "world"',[]))
-        #test_cases.append((r'Select "hello", "world", "hello\" world", "hello world\\\\\\", "hello world\\" order by "world"',[]))
 
         for tc in test_cases:
             format_expression, string_literals = replace_string_literals(tc[0])
             canonic_literals = tc[1]
             self.assertEqual(canonic_literals, string_literals)
 
-
+    def test_separate_actions(self):
+        query = 'select top   100 *, a2, a3 inner  join /path/to/the/file.tsv on a1 == b3 where a4 == "hello" and int(b3) == 100 order by int(a7) desc '
+        canonic_res = {'INNER JOIN': {'text': '/path/to/the/file.tsv on a1 == b3'}, 'SELECT TOP': {'text': '*, a2, a3', 'top': 100}, 'WHERE': {'text': 'a4 == "hello" and int(b3) == 100'}, 'ORDER BY': {'text': 'int(a7)', 'reverse': True}}
+        test_res = separate_actions(query)
+        assert test_res == canonic_res
